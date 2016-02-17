23b29954076d782b2636153c8d1fe1e33373c85f
Severus256 <severus256@gmail.com>
2016-02-17 16:24:36 +0100

    get root element XML

--- src/domparser/DOMParser.java
+++ src/domparser/DOMParser.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.SAXException;
 
 /**
@@ -26,6 +27,10 @@ public class DOMParser {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document doc = builder.parse(new File("src/domparser/config.xml"));
+        Element element = doc.getDocumentElement();
+        String rootElement = element.getNodeName();   // получаем имя элемента (рутовский)
+        System.out.println(rootElement);
+        
     }
 
 }
