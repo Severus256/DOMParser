105d2edb983cc9c2920473b98626657c9218c3f5
Severus256 <severus256@gmail.com>
2016-02-17 16:21:25 +0100

    parse parse parse

--- src/domparser/DOMParser.java
+++ src/domparser/DOMParser.java
@@ -5,6 +5,14 @@
  */
 package domparser;
 
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author Aga
@@ -14,8 +22,10 @@ public class DOMParser {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File("src/domparser/config.xml"));
     }
-    
+
 }
