312514136e523bbc2ff737c6b2395e71dcd5d1a9
Severus256 <severus256@gmail.com>
2016-02-17 16:46:58 +0100

    it's work

--- src/domparser/DOMParser.java
+++ src/domparser/DOMParser.java
@@ -12,6 +12,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 /**
@@ -27,10 +28,27 @@ public class DOMParser {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document doc = builder.parse(new File("src/domparser/config.xml"));
-        Element element = doc.getDocumentElement();
-        String rootElement = element.getNodeName();   // получаем имя элемента (рутовский)
-        System.out.println(rootElement);
+      //  String element = doc.getDocumentElement().getNodeName();
+       // System.out.println("Root: " + element);
+
+        // NodeList хранит список node объектов, каждый из которых - элемент XML документа
+        NodeList list = doc.getElementsByTagName("product");
         
+        String title="";
+        String price = "";
+        int amount = 0;
+        
+        // выпишем список всех атрибутов продуктов из XML
+        for (int i = 0; i <list.getLength(); i++) {
+            Element element = (Element)list.item(i);
+            title =  element.getElementsByTagName("title").item(0).getChildNodes().item(0).getNodeValue(); // получаем самый первый node из nodelist у title
+            price =  element.getElementsByTagName("price").item(0).getChildNodes().item(0).getNodeValue();
+            amount =  (Integer.parseInt(element.getElementsByTagName("amount").item(0).getChildNodes().item(0).getNodeValue()));
+            System.out.println("Название продукта: "+title+"\n");
+            System.out.println("Цена продукта: "+price+"\n");
+            System.out.println("Количество: "+amount+"\n");
+        }
+
     }
 
 }
